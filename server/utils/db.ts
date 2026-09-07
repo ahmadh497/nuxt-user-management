afbf8d65dd7c0371a67d49c96b15ea921ef6c35c
Ahmad Hassan Haidar <ah2202630@gmail.com>
2026-09-08 00:07:56 +0300

    Prepare MySQL connection for deployment

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -1,8 +1,11 @@
 import mysql from 'mysql2/promise'
 
 export const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'vue_learning'
+  host: process.env.MYSQLHOST || 'localhost',
+  port: Number(process.env.MYSQLPORT || 3306),
+  user: process.env.MYSQLUSER || 'root',
+  password: process.env.MYSQLPASSWORD || '',
+  database: process.env.MYSQLDATABASE || 'vue_learning',
+  waitForConnections: true,
+  connectionLimit: 10
 })
